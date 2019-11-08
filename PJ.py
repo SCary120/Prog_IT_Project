30bd35e4c0fcd85359ac09ca0336bcb340ecd80a
SCary120 <55851142+SCary120@users.noreply.github.com>
2019-11-08 09:57:13 -0500

    Update PJ.py

--- PJ.py
+++ PJ.py
@@ -1,90 +1,67 @@
-# Nested dictionary for different species of fish.
-fish = {"Tangs": {"1":"Yellow", "2":"Kole", "3":"Blue", "4":"Powder Blue", "5":"Powder Brown", "6":"Naso",
-"7":"Blonde Naso", "8":"Sailfin", "9":"Scopas", "10":"Clown", "11":"Convict", "12":"Achilles", "13":"Purple","14":"Chevron", 
-"15":"Lieutenant","16":"Yellow Belly Blue Regal", "17":"Blue Carribean", "18":"Gemmatum","19":"Black Longnose", "20":"Two Spot Bristletooth",
-"21":"Orange Shoulder","22":"Square Tail Bristletooth","23":"Whitetail Bristletooth", "24":"Mimic Lemon Peel", "25":"Vlamingii"},
-"Dragonets":{"1":"Green Mandarin", "2":"Spotted Mandarin", "3":"Red Mandarin", "4":"Scooter Blenny", "5":"Ruby Red Dragonet", 
-"6":"Red Scooter Dragonet"}"Chromis":{"1":"Blue/Green Reef","2":"Black Axil","3":"Lemon","4":"Blue Reef","5":"Black & White","6":"Agile","7":"Sunshine Purple & Yellow","8":"Webers"},
-"Clowns":{"1":"Mocha Black Storm","2":"Black Storm","3":"Latezonatus","4":"Ocellaris","5":"Picasso Percula","6":"Semi Picasso","7":"Wyoming White",
-"8":"Davinci","9":"MochaVinci","10":"Wide Bar Mocha Gladiator","11":"Clarkii","12":"Black & White Ocellaris","13":"Snow Onyx","14":"Black Onyx",
-"15":"Snowflake Ocellaris","16":"Black Snowflake Phantom","17":"Maine Blizzard","18":"Flurry","19":"Frostbite","20":"Black Ice","21":"Black Photon",
-"22":"Naked","23":"Nearly Naked","24":"Midnight Ocellaris","25":"Domino","26":"Maroon","27":"Gold Nugget","28":"Gold Dot","29":"Lightning Maroon",
-"30":"Thunder Maroon","31":"YellowstripeMaroon","32":"Cinnamon","33":"Spotcinctus","34":"Pink Skunk","35":"Red Saddle","36":"Tomato","37":"Fiji Barberi",
-"38":"Galaxy Clarkii","39":"Pearl Eye Clarkii","40":"Two Banded","41":"Saddleback"},"Gobies":{"1":"Yello Prawn","2":"Court Jester","3":"Diamond Watchman",
-"4":"Pink Spotted","5":"Tiger Watchman","6":"Hi Fin Red Banded","7":"Sleeper Banded","8":"Sleeper Blue Dot","9":"Sleeper Gold Head","10":"Yellow Clown",
-"11":"Green Clown","12":"Black Clown","13":"Wheelers Shrimp","14":"Bella","15":"Flagtail Shrimp","16":"Bluespotted Watchman","17":"Greenbanded",
-"18":"Sleeper Railway Glider","19":"Sleeper Striped","20":"Citrinis Clown","21":"Red Neon Eviota","22":"Yasha White Ray Shrimp","23":"Wide-barred Shrimp",
-"24":"Orange Stripe Prawn","25":"Neon Blue","26":"Gold Neon Eviota","27":"Two Spot","28":"Orange Spotted","29":"Engineer","30":"Hectors","31":"Pinkbar",
-"32":"Red Head","33":"Cave Transparent","34":"Steinitz","35":"Orangemarked","36":"Tangaroa","37":"Panda","38":"Masked","39":"Black Barred Convict",
-"40":"Yellow Priolepis","41":"Dracula","42":"Decorated","43":"Broadstripe"},"Angel":{"1":"Asfur","2":"Blue Girdled","3":"Blueline","4":"Goldflake",
-"5":"Regal","6":"Bellus","7":"Scribbled","8":"Passer","9":"Koran","10":"Emperor","11":"Queen","12":"Blueface","13":"Annularis","14":"French","15":"Rock Beauty",
-"16":"Spotbreast","17":"Lamarcks","18":"Watanabei","19":"Singapore","20":"Gray","21":"Xanthurus Cream","22":"Flagfin","23":"False Personifer","24":"Chrysurus",
-"25":"Maculosus","26":"Cortez","27":"Zebra Lyretail"},"Dwarf Angel":{"1":"Flame","2":"Coral Beauty","3":"Yellow","4":"Tibicen","5":"Joculator",
-"6":"True Shepards","7":"Bicolor","8":"Lemonpeel","9":"Yellowfin","10":"Flameback","11":"Collins","12":"Potters","13":"Red Stripe","14":"Rusty",
-"15":"Half Black","16":"Bluefin","17":"Pygmy","18":"Black Nox","19":"Multicolor","20":"Golden"},"Anglers":{"1":"Assorted","2":"Wartskin","3":"Red/Orange",
-"4":"Sargassum"}, "Anthias":{"1":"Dispar","2":"Bartlett","3":"Lyretail","4":"Indian Ocean Lyretail","5":"Carberryi","6":"Stocky","7":"Evansi","8":"Sunset",
+#Nested dictionary for different species of fish.
+fish = {"Tangs": {"1":"Yellow", "2":"Kole", "3":"Blue", "4":"Powder Blue", "5":"Powder Brown", "6":"Naso","7":"Blonde Naso", "8":"Sailfin", "9":"Scopas",
+"10":"Clown", "11":"Convict", "12":"Achilles", "13":"Purple","14":"Chevron", "15":"Lieutenant","16":"Yellow Belly Blue Regal", "17":"Blue Carribean",
+"18":"Gemmatum","19":"Black Longnose", "20":"Two Spot Bristletooth", "21":"Orange Shoulder","22":"Square Tail Bristletooth","23":"Whitetail Bristletooth",
+"24":"Mimic Lemon Peel", "25":"Vlamingii"},"Dragonets":{"1":"Green Mandarin", "2":"Spotted Mandarin", "3":"Red Mandarin", "4":"Scooter Blenny",
+"5":"Ruby Red Dragonet", "6":"Red Scooter Dragonet"},"Chromis":{"1":"Blue/Green Reef","2":"Black Axil","3":"Lemon","4":"Blue Reef","5":"Black & White",
+"6":"Agile","7":"Sunshine Purple & Yellow","8":"Webers"},"Clowns":{"1":"Mocha Black Storm","2":"Black Storm","3":"Latezonatus","4":"Ocellaris",
+"5":"Picasso Percula","6":"Semi Picasso","7":"Wyoming White","8":"Davinci","9":"MochaVinci","10":"Wide Bar Mocha Gladiator","11":"Clarkii",
+"12":"Black & White Ocellaris","13":"Snow Onyx","14":"Black Onyx","15":"Snowflake Ocellaris","16":"Black Snowflake Phantom","17":"Maine Blizzard",
+"18":"Flurry","19":"Frostbite","20":"Black Ice","21":"Black Photon","22":"Naked","23":"Nearly Naked","24":"Midnight Ocellaris","25":"Domino",
+"26":"Maroon","27":"Gold Nugget","28":"Gold Dot","29":"Lightning Maroon","30":"Thunder Maroon","31":"YellowstripeMaroon","32":"Cinnamon","33":"Spotcinctus",
+"34":"Pink Skunk","35":"Red Saddle","36":"Tomato","37":"Fiji Barberi","38":"Galaxy Clarkii","39":"Pearl Eye Clarkii","40":"Two Banded","41":"Saddleback"},
+"Gobies":{"1":"Yello Prawn","2":"Court Jester","3":"Diamond Watchman","4":"Pink Spotted","5":"Tiger Watchman","6":"Hi Fin Red Banded","7":"Sleeper Banded",
+"8":"Sleeper Blue Dot","9":"Sleeper Gold Head","10":"Yellow Clown","11":"Green Clown","12":"Black Clown","13":"Wheelers Shrimp","14":"Bella",
+"15":"Flagtail Shrimp","16":"Bluespotted Watchman","17":"Greenbanded","18":"Sleeper Railway Glider","19":"Sleeper Striped","20":"Citrinis Clown",
+"21":"Red Neon Eviota","22":"Yasha White Ray Shrimp","23":"Wide-barred Shrimp","24":"Orange Stripe Prawn","25":"Neon Blue","26":"Gold Neon Eviota",
+"27":"Two Spot","28":"Orange Spotted","29":"Engineer","30":"Hectors","31":"Pinkbar","32":"Red Head","33":"Cave Transparent","34":"Steinitz",
+"35":"Orangemarked","36":"Tangaroa","37":"Panda","38":"Masked","39":"Black Barred Convict","40":"Yellow Priolepis","41":"Dracula","42":"Decorated",
+"43":"Broadstripe"},"Angel":{"1":"Asfur","2":"Blue Girdled","3":"Blueline","4":"Goldflake","5":"Regal","6":"Bellus","7":"Scribbled","8":"Passer","9":"Koran",
+"10":"Emperor","11":"Queen","12":"Blueface","13":"Annularis","14":"French","15":"Rock Beauty","16":"Spotbreast","17":"Lamarcks","18":"Watanabei",
+"19":"Singapore","20":"Gray","21":"Xanthurus Cream","22":"Flagfin","23":"False Personifer","24":"Chrysurus","25":"Maculosus","26":"Cortez",
+"27":"Zebra Lyretail"},"Dwarf Angel":{"1":"Flame","2":"Coral Beauty","3":"Yellow","4":"Tibicen","5":"Joculator","6":"True Shepards","7":"Bicolor",
+"8":"Lemonpeel","9":"Yellowfin","10":"Flameback","11":"Collins","12":"Potters","13":"Red Stripe","14":"Rusty","15":"Half Black","16":"Bluefin","17":"Pygmy",
+"18":"Black Nox","19":"Multicolor","20":"Golden"},"Anglers":{"1":"Assorted","2":"Wartskin","3":"Red/Orange","4":"Sargassum"},
+"Anthias":{"1":"Dispar","2":"Bartlett","3":"Lyretail","4":"Indian Ocean Lyretail","5":"Carberryi","6":"Stocky","7":"Evansi","8":"Sunset",
 "9":"Pictilis","10":"Ignitus","11":"Squareback","12":"Resplendent","13":"Randalls","14":"Huchtii","15":"Red Belted","16":"Bimaculatus","17":"Fathead Sunburst",
 "18":"Red Saddled","19":"Princess","20":"Waitei","21":"Loris","22":"Red Fairy","23":"Bali Lunate","24":"Red-Bar"}, "Basslets":{"1":"Royal Gramma",
-"2":"Chalk Bass","3":"Lantern Bass","4":"Tobacco","5":"Harlequin","6":"Black Cap","7":"Gold Assessor"},}
+"2":"Chalk Bass","3":"Lantern Bass","4":"Tobacco","5":"Harlequin","6":"Black Cap","7":"Gold Assessor"},"Blennies":{"1":"Sailfin/Algae","2":"Bicolor",
+"3":"Midas","4":"Tail Spot","5":"Linear","6":"Two Spot","7":"Black Sailfin","8":"Starry","9":"Black Combtooth","10":"Canary","11":"Green Canary",
+"12":"Forktail","13":"Yellow Eye Combtooth","14":"Segmented Sailfin","15":"One Spot","16":"Striped","17":"White","18":"Harptail","19":"Black-Lined",
+"20":"Ember","21":"Bundoon","22":"Brachiosaurus","23":"Eyebrow"},"Butterfly":{"1":"Auriga","2":"Four Eyed","3":"Pakistan","4":"Declivis","5":"Saddleback",
+"6":"Falcula","7":"Raccoon","8":"Orange","9":"Mertensii","10":"Spotfin","11":"Sunset","12":"Latticed","13":"Banded","14":"Tahitian","15":"Double Saddle",
+"16":"Tear Drop","17":"Vagabond","18":"Pearlscale","19":"Copperband","20":"Yellow Longnose","21":"Zoster","22":"Heniochus","23":"Red Sea",
+"24":"Singular","25":"Hi Fin Coradion","26":"Indian Vegabond","27":"Yellowhead"},"Boxfish":{"1":"Longhorn Cowfish","2":"Cubicus"},
+"Cardinal":{"1":"Pajama","2":"Red Belted","3":"Longspine","4":"Yellowstriped","5":"Flame","6":"Fowleria","7":"Ochrestriped","8":"Five Lined",
+"9":"Yellow","10":"Bangai"},"Damsel":{"1":"Ambon","2":"Yellowtail","3":"Azure","4":"Blue","5":"Blue Sapphire","6":"Three Spot Domino","7":"Four Stripe",
+"8":"Three Stripe","9":"Fiji Blue Devil","10":"Two Stripe","11":"Blue and Gold","12":"Talbots","13":"Blue Velvet","14":"Pink Smith","15":"Neon",
+"16":"Rollands","17":"Regal","18":"Similar","19":"Traceys"},"Dottyback":{"1":"Pruple","2":"Purple Stripe","3":"Bicolor","4":"Splendid","5":"Whitetail",
+"6":"Ring Eyed","7":"Yellow","8":"Red Spotted","9":"Neon","10":"Springeri","11":"Double Striped","12":"Blue Eye Royal","13":"Orangetail",
+"14":"Red Elongated","15":"Black Neon","16":"Splendid"},"Hawkfish":{"1":"Falco","2":"Flame","3":"Pixy/Spotted","4":"Longnose",
+"5":"Arc Eye","6":"Freckled","7":"Red","8":"Blood Red"},"Wrasse Reef Safe":{"1":"Darwins Glow","2":"Magma Fairy","3":"Splendid Pintail",
+"4":"Katherines Fairy","5":"Lunate Fairy","6":"Ornate Leopard","7":"Blue Throat","8":"Naokos Fairy","9":"Rose-Band Fairy","10":"Red-Fin Fairy",
+"11":"Redfin","12":"Leopard","13":"Black Leopard","14":"Four Line","15":"Six Line","16":"Hooded Fairy","17":"Orange-Back Fairy","18":"Ruby Head Fairy",
+"19":"Scotts Fairy","20":"Bluehead Fairy","21":"Exquisite Fairy","22":"Whip Fin Fairy","23":"Yellow Fin Fairy","24":"Johnsons Fairy","25":"Flame Fairy",
+"26":"Labouts Fairy","27":"Lineatus Fairy","28":"Multicolor Lubbocks Fairy","29":"Yellow-Flanked Fairy","30":"Fine Spotted Fairy",
+"31":"Golden Rhomboidalis","32":"Pink Margin Fairy","33":"Red Velvet Fairy","34":"Red Head Solon Fairy","35":"Pearly Wrasse","36":"Bluestreak Cleaner",
+"37":"Ruby Longfin Fairy","38":"McCoskers Flasher","39":"Carpenters Flasher","40":"Yellowfin Flasher","41":"Blue Flasher","42":"Linespot Flahser",
+"43":"Filamented Flasher","44":"Mystery","45":"Yellowband","46":"Pink-Streaked","47":"Scarlet Pin Stripe","48":"Eight Lined"},
+"Foxface & Rabbit":{"1":"Foxface Lo","2":"One Spot Foxface","3":"Magnificent Foxface","4":"Yellow Blotch Rabbit","5":"Blue Spotted Rabbit",
+"6":"Two Barred Rabbit","7":"Decorated Rabbit","8":"Gold Spotted Rabbit","9":"Bicolor Fox"},"Filefish":{"1":"Aiptasia Eating","2":"Tassle"},
+"Groupers":{"1":"Blue Line","2":"Miniatus","3":"Pollenei"},"Hogfish":{"1":"Yellow Candy","2":"Spanish","3":"Axilspot","4":"Red Diana","5":"Coral"},
+"Jawfish":{"1":"Blue Dot","2":"Yellowhead","3":"Black Cap","4":"Chinstrap"},"Wrasse Fish Only":{"1":"Harlequin Tusk","2":"Yellow","3":"Yellow & Purple",
+"4":"Green","5":"Red Coris","6":"Formosa","7":"Melanurus","8":"Radiant","9":"Christmas","10":"Vroliks","11":"Marble/Hortulanus","12":"Grey Head",
+"13":"Adorned","14":"Lyretail","15":"Bluehead","16":"Banana","17":"Bird","18":"Dragon","19":"Hardwicke","20":"Paddlefin","21":"Pinkface","22":"Neon",
+"23":"Goldbar","24":"Blunthead","25":"Red Breast","26":"Black-backed","27":"Red-Lined","28":"Two Spot","29":"Dusky","30":"Two Tone","31":"Richmonds",
+"32":"Jansen Saddle","33":"Sea Grass","34":"Redtail Hawaii"},"Dartfish":{"1":"Firefish","2":"Purple Firefish","3":"Scissortail","4":"Zebra Barred",
+"5":"Blue Gudgeon","6":"Exquisite Firefish","7":"Helfrichi Firefish"},"Puffers":{"1":"Saddle Valentini","2":"Porcupine","3":"Arothron","4":"Arothron Dog Face",
+"5":"Saddle","6":"Scribbled Arothron","7":"Spiny Box","8":"Blue Spotted","9":"Spotted","10":"Central American Sharpnose","11":"Leopard",
+"12":"Bennetts Sharpnose","13":"Immaculatus","14":"Narrow-Lined","15":"Stars & Stripes","16":"Reticulated"},"Lionfish":{"1":"Dwarf/Zebra","2":"Fuzzy Dwarf",
+"3":"Colored Volitan","4":"Miles","5":"Antennata","6":"Radiata","7":"Russells","8":"Mombasa"},"Eels":{"1":"Golden Dwarf Moray","2":"Snowflake","3":"Tessalata",
+"4":"Wolf","5":"Zebra Moray","6":"Golden Banana Moray","7":"Black Edge Moray","8":"Jeweled Moray","9":"Japanese Dragon","10":"Yellow-Edged"}
+}
 
 
-
-# Dictionary of Blenny fish.
-"Blennies":{"Sailfin/Algae","Bicolor","Midas","Tail Spot","Linear","Two Spot","Black Sailfin","Starry","Black Combtooth",
-"Canary","Green Canary","Forktail","Yellow Eye Combtooth","Segmented Sailfin","One Spot","Striped","White","Harptail",
-"Black-Lined","Ember","Bundoon","Brachiosaurus","Eyebrow"},
-# Dictionary of Butterfly fish.
-"Butterfly":{"Auriga","Four Eyed","Pakistan","Declivis","Saddleback","Falcula","Raccoon","Orange","Mertensii","Spotfin",
-"Sunset","Latticed","Banded","Tahitian","Double Saddle","Tear Drop","Vagabond","Pearlscale","Copperband","Yellow Longnose",
-"Zoster","Heniochus","Red Sea","Singular","Hi Fin Coradion","Indian Vegabond","Yellowhead"},
-# Dictionary of Boxfish.
-"Boxfish":{"Longhorn Cowfish","Cubicus"},
-# Dictionary of Cardinal fish.
-"Cardinal":{"Pajama","Red Belted","Longspine","Yellowstriped","Flame","Fowleria","Ochrestriped","Five Lined","Yellow","Bangai"},
-# Dictionary of Damsel fish.
-"Damsel":{"Ambon","Yellowtail","Azure","Blue","Blue Sapphire","Three Spot Domino","Four Stripe","Three Stripe",
-"Fiji Blue Devil","Two Stripe","Blue and Gold","Talbots","Blue Velvet","Pink Smith","Neon","Rollands","Regal","Similar","Traceys"},
-# Dictionary of Dottyback fish.
-"Dottyback":{"Pruple","Purple Stripe","Bicolor","Splendid","Whitetail","Ring Eyed","Yellow","Red Spotted","Neon","Springeri",
-"Double Striped","Blue Eye Royal","Orangetail","Red Elongated","Black Neon","Splendid"},
-# Dictionary of Hawkfish.
-"Hawkfish":{"Falco","Flame","Pixy/Spotted","Longnose","Arc Eye","Freckled","Red","Blood Red"},
-# Dictionary of Reef Safe Wrasse fish.
-"Wrasse Reef Safe":{"Darwins Glow","Magma Fairy","Splendid Pintail","Katherines Fairy","Lunate Fairy","Ornate Leopard",
-"Blue Throat","Naokos Fairy","Rose-Band Fairy","Red-Fin Fairy","Redfin","Leopard","Black Leopard","Four Line","Six Line",
-"Hooded Fairy","Orange-Back Fairy","Ruby Head Fairy","Scotts Fairy","Bluehead Fairy","Exquisite Fairy","Whip Fin Fairy",
-"Yellow Fin Fairy","Johnsons Fairy","Flame Fairy","Labouts Fairy","Lineatus Fairy","Multicolor Lubbocks Fairy",
-"Yellow-Flanked Fairy","Fine Spotted Fairy","Golden Rhomboidalis","Pink Margin Fairy","Red Velvet Fairy","Red Head Solon Fairy",
-"Pearly Wrasse","Bluestreak Cleaner","Ruby Longfin Fairy","McCoskers Flasher","Carpenters Flasher","Yellowfin Flasher",
-"Blue Flasher","Linespot Flahser","Filamented Flasher","Mystery","Yellowband","Pink-Streaked","Scarlet Pin Stripe","Eight Lined"},
-# Dictionary of Wrasse fish.
-"Wrasse Fish Only":{"Harlequin Tusk","Yellow","Yellow & Purple","Green","Red Coris","Formosa","Melanurus","Radiant",
-"Christmas","Vroliks","Marble/Hortulanus","Grey Head","Adorned","Lyretail","Bluehead","Banana","Bird","Dragon",
-"Hardwicke","Paddlefin","Pinkface","Neon","Goldbar","Blunthead","Red Breast","Black-backed","Red-Lined","Two Spot","Dusky",
-"Two Tone","Richmonds","Jansen Saddle","Sea Grass","Redtail Hawaii"},
-# Dictionary of Dartfish.
-"Dartfish":{"Firefish","Purple Firefish","Scissortail","Zebra Barred","Blue Gudgeon","Exquisite Firefish","Helfrichi Firefish"},
-# Dictionary of Puffers.
-"Puffers":{"Saddle Valentini","Porcupine","Arothron","Arothron Dog Face","Saddle","Scribbled Arothron","Spiny Box",
-"Blue Spotted","Spotted","Central American Sharpnose","Leopard","Bennetts Sharpnose","Immaculatus","Narrow-Lined",
-"Stars & Stripes","Reticulated"},
-# Dictionary of Lionfish.
-"Lionfish":{"Dwarf/Zebra","Fuzzy Dwarf","Colored Volitan","Miles","Antennata","Radiata","Russells","Mombasa"},
-# Dictionary of Eels.
-"Eels":{"Golden Dwarf Moray","Snowflake","Tessalata","Wolf","Zebra Moray","Golden Banana Moray","Black Edge Moray",
-"Jeweled Moray","Japanese Dragon","Yellow-Edged"},
-# Dictionary of Foxface & Rabbit fish.
-"Foxface & Rabbit":{"Foxface Lo","One Spot Foxface","Magnificent Foxface","Yellow Blotch Rabbit","Blue Spotted Rabbit",
-"Two Barred Rabbit","Decorated Rabbit","Gold Spotted Rabbit","Bicolor Fox"},
-# Dictionary of Filefish.
-"Filefish":{"Aiptasia Eating","Tassle"},
-# Dictionary of Groupers.
-"Groupers":{"Blue Line","Miniatus","Pollenei"},
-# Dictionary of Hogfish.
-"Hogfish":{"Yellow Candy","Spanish","Axilspot","Red Diana","Coral"},
-# Dictionary of Jawfish.
-"Jawfish":{"Blue Dot","Yellowhead","Black Cap","Chinstrap"},
-        }
 # Dictionary for different species of coral.
 coral = {
 # Dictionary of LPS coral.
